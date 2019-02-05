a4315cfb4903f01c7d88538beeeed789d2c3b51d
github username <github email address>
2019-02-05 09:36:37 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates :content, length: { minimum: 250 }
+
 end
+
+# create_table "posts", force: :cascade do |t|
+#   t.string   "title"
+#   t.string   "category"
+#   t.text     "content"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+# end
